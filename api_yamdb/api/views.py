ef697521f0f2913c4d037de35d038b926bf6b072
Chiken-Kitchen <filkargin@yandex.ru>
2024-10-15 11:48:12 +0300

    fix errors and add model

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters, viewsets
 
 from .models import Category, Genre
 from .serializers import CategorySerializer, GenreSerializer
@@ -7,8 +8,12 @@ from .serializers import CategorySerializer, GenreSerializer
 class GenreViewSet(viewsets.ModelViewSet):
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
+    filter_backends = (DjangoFilterBackend, filters.SearchFilter)
+    search_fields = ('name')
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    filter_backends = (DjangoFilterBackend, filters.SearchFilter)
+    search_fields = ('name')
