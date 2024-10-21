9e4f0ca458b7f97e5c3acb229ce2f80f661a2f0c
Daniil Tropin <Rockur@ya.ru>
2024-10-21 11:13:37 +0300

    Add version of router. Now custom users near with general api url

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -6,24 +6,27 @@ from .views import (
     GenreViewSet,
     TitleViewSet,
     ReviewViewSet,
-    CommentViewSet
+    CommentViewSet,
 )
 
 app_name = 'api'
 
-router = DefaultRouter()
-router.register('genres', GenreViewSet, basename='genres')
-router.register('categories', CategoryViewSet, basename='categories')
-router.register('titles', TitleViewSet, basename='titles')
-router.register(
-    r'titles/(?P<title_id>\d+)/reviews', ReviewViewSet, basename='reviews'
+router_v1 = DefaultRouter()
+router_v1.register('genres', GenreViewSet, basename='genres')
+router_v1.register('categories', CategoryViewSet, basename='categories')
+router_v1.register('titles', TitleViewSet, basename='titles')
+router_v1.register(
+    r'titles/(?P<title_id>\d+)/reviews',
+    ReviewViewSet,
+    basename='reviews',
 )
-router.register(
+router_v1.register(
     r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
     CommentViewSet,
     basename='comments',
 )
 
 urlpatterns = [
-    path('v1/', include(router.urls)),
+    path('v1/', include(router_v1.urls)),
+    path('v1/', include('users.urls')),
 ]
