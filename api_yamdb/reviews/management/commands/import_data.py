4f68c00d12d212e049513b84b240239f459c97d2
Chiken-Kitchen <filkargin@yandex.ru>
2024-10-22 21:24:59 +0300

    add import_users, reviews, comments; change model Comment (add author, pub_date) add into field title null=True

--- api_yamdb/reviews/management/commands/import_data.py
+++ api_yamdb/reviews/management/commands/import_data.py
@@ -4,7 +4,9 @@ import pandas as pd
 from django.core.management.base import BaseCommand
 from django.conf import settings
 
-from reviews.models import Category, Genre, Title
+
+from reviews.models import Category, Comment, Genre, Review, Title
+from users.models import CustomUser
 
 
 def get_data_frame(data_dir, filename) -> pd.DataFrame:
@@ -24,6 +26,9 @@ class Command(BaseCommand):
         self.import_genres(data_dir)
         self.import_titles(data_dir)
         self.import_genre_titles(data_dir)
+        self.import_users(data_dir)
+        self.import_reviews(data_dir)
+        self.import_comments(data_dir)
 
     def import_categories(self, data_dir) -> None:
         """Import categories into database."""
@@ -31,6 +36,7 @@ class Command(BaseCommand):
         df = get_data_frame(data_dir, 'category.csv')
         for _, row in df.iterrows():
             Category.objects.get_or_create(
+                id=row['id'],
                 name=row['name'],
                 slug=row['slug'],
             )
@@ -41,6 +47,7 @@ class Command(BaseCommand):
         df = get_data_frame(data_dir, 'genre.csv')
         for _, row in df.iterrows():
             Genre.objects.get_or_create(
+                id=row['id'],
                 name=row['name'],
                 slug=row['slug'],
             )
@@ -52,6 +59,7 @@ class Command(BaseCommand):
         for _, row in df.iterrows():
             category = Category.objects.get(id=row['category'])
             Title.objects.get_or_create(
+                id=row['id'],
                 name=row['name'],
                 year=row['year'],
                 category=category,
@@ -65,3 +73,53 @@ class Command(BaseCommand):
             title = Title.objects.get(id=row['title_id'])
             genre = Genre.objects.get(id=row['genre_id'])
             title.genres.add(genre)
+
+    def import_users(self, data_dir):
+        """Import users into database."""
+
+        df = get_data_frame(data_dir, 'users.csv')
+        for _, row in df.iterrows():
+            CustomUser.objects.get_or_create(
+                id=row['id'],
+                username=row['username'],
+                email=row['email'],
+                role=row['role'],
+                bio=row['bio'],
+                first_name=row['first_name'],
+                last_name=row['last_name']
+            )
+
+    def import_reviews(self, data_dir):
+        """Import reviews into database."""
+
+        df = get_data_frame(data_dir, 'review.csv')
+        for _, row in df.iterrows():
+            title = Title.objects.get(id=row['title_id'])
+            author = CustomUser.objects.get(id=row['author'])
+            Review.objects.get_or_create(
+                id=row['id'],
+                defaults={
+                    'title': title,
+                    'text': row['text'],
+                    'author': author,
+                    'score': row['score'],
+                    'pub_date': row['pub_date']
+                }
+            )
+
+    def import_comments(self, data_dir):
+        """Import comments into database."""
+
+        df = get_data_frame(data_dir, 'comments.csv')
+        for _, row in df.iterrows():
+            review = Review.objects.get(id=row['review_id'])
+            author = CustomUser.objects.get(id=row['author'])
+            Comment.objects.get_or_create(
+                id=row['id'],
+                review=review,
+                defaults={
+                    'text': row['text'],
+                    'pub_date': row['pub_date'],
+                    'author': author,
+                }
+            )
