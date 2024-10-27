5d1160c07da0561805a616298865ce0e640a328d
Daniil Tropin <Rockur@ya.ru>
2024-10-28 01:37:28 +0300

    Clean code, refactor method 'me', remove permissions

--- api_yamdb/users/views.py
+++ api_yamdb/users/views.py
@@ -1,21 +1,16 @@
-from django.forms import ValidationError
 from django.shortcuts import get_object_or_404
 from django.conf import settings
 from django.core.mail import send_mail
 from django.contrib.auth.tokens import default_token_generator
 from django.contrib.auth import get_user_model
 from rest_framework import status, viewsets
-from rest_framework.decorators import api_view, permission_classes, action
+from rest_framework.decorators import api_view, action
 from rest_framework.response import Response
 from rest_framework.request import Request
 from rest_framework_simplejwt.tokens import AccessToken
-from rest_framework.permissions import AllowAny, IsAuthenticated
-from rest_framework.exceptions import ValidationError
+from rest_framework.permissions import IsAuthenticated
 
-from .permissions import (
-    IsSuperuserOrAdmin,
-    IsOwnerOrReadOnly
-)
+from .permissions import IsSuperuserOrAdmin
 
 from .serializers import (
     UserAccessTokenSerializer,
@@ -32,48 +27,36 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [IsSuperuserOrAdmin]
+    http_method_names = ['get', 'post', 'patch', 'delete']
     lookup_field = 'username'
-    search_fields = ('role',)
+    search_fields = ('role', 'username')
 
-    def create(self, request, *args, **kwargs):
-        email = request.data.get('email')
-        if User.objects.filter(email=email).exists():
-            raise ValidationError(
-                {'email': 'User with this Email already exists.'}
-            )
-        return super().create(request, *args, **kwargs)
-
-    def get_queryset(self):
-        queryset = super().get_queryset()
-        search_param = self.request.query_params.get('search', None)
-        if search_param:
-            queryset = queryset.filter(username__icontains=search_param)
-        return queryset
-
-    def update(self, request, *args, **kwargs):
-        if request.method == 'PUT':
-            return Response(
-                {'detail': 'Method Not Allowed'},
-                status=status.HTTP_405_METHOD_NOT_ALLOWED
-            )
-        return super().update(request, *args, **kwargs)
-
-    @action(methods=['patch', 'get'],
-            detail=False, url_path='me', permission_classes=[IsAuthenticated])
+    @action(
+        detail=False,
+        url_path='me',
+        methods=['GET', 'PATCH'],
+        permission_classes=[IsAuthenticated],
+    )
     def me(self, request: Request) -> Response:
         if request.method == 'GET':
             serializer = UserSerializer(self.request.user)
-            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            data = request.data.copy()
 
-        serializer = UserSerializer(self.request.user, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save(partial=True)
+            # Prevent role from being changed
+            if 'role' in data and request.user.role != data['role']:
+                data.pop('role')
+
+            serializer = UserSerializer(
+                self.request.user, data=data, partial=True
+            )
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
 
         return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 @api_view(['POST'])
-@permission_classes([AllowAny])
 def get_jwt_token(request: Request) -> Response:
     serializer = UserAccessTokenSerializer(data=request.data)
     serializer.is_valid(raise_exception=True)
@@ -87,12 +70,11 @@ def get_jwt_token(request: Request) -> Response:
 
 
 @api_view(['POST'])
-@permission_classes([AllowAny])
 def signup(request: Request) -> Response:
     serializer = UserCreationSerializer(data=request.data)
     serializer.is_valid(raise_exception=True)
 
-    # If the user not exists and requests confirmation code
+    # If the user does not exists and requests confirmation code
     user = serializer.get_or_create()
 
     confirmation_code = default_token_generator.make_token(user)
