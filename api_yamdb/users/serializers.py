80a362c850eb2be21152ff15df692edc40006e52
Daniil Tropin <Rockur@ya.ru>
2024-10-28 01:42:52 +0300

    Do username and email validatation logic easier. Move validatation logic to BaseUserSerializer

--- api_yamdb/users/serializers.py
+++ api_yamdb/users/serializers.py
@@ -1,14 +1,75 @@
 import re
+from typing import OrderedDict
 
 from django.contrib.auth import get_user_model
 from django.contrib.auth.tokens import default_token_generator
 from django.shortcuts import get_object_or_404
 from rest_framework import serializers
 
+from .models import CustomUser
+
 User = get_user_model()
 
 
-class UserSerializer(serializers.ModelSerializer):
+class BaseUserSerializer(serializers.ModelSerializer):
+    """
+    A base class for user properties and methods.
+    This class does not inherit from models.Model, so no extra table is created.
+    """
+
+    email = serializers.EmailField(required=True)
+    username = serializers.CharField(required=True)
+
+    def validate_email(self, value: str) -> str:
+        if len(value) > 254:
+            raise serializers.ValidationError(
+                {'email': 'Choose another email'}
+            )
+        return value
+
+    def validate_username(self, value: str) -> str:
+        if len(value) > 150 or value == 'me':
+            raise serializers.ValidationError(
+                {'username': 'Choose another username'}
+            )
+
+        if not re.match(r'^[\w.@+-]+$', value):
+            raise serializers.ValidationError(
+                'Username contains not allowed symbols'
+            )
+
+        return value
+
+    def validate(self, data: OrderedDict) -> OrderedDict:
+        username = data.get('username')
+        email = data.get('email')
+
+        # Check if any user exists with the same username but different email
+        if (
+            User.objects.filter(username=username)
+            .exclude(email=email)
+            .exists()
+        ):
+            raise serializers.ValidationError(
+                {'username': 'Choose another username.'}
+            )
+
+        # Check if any user exists with the same email but different username
+        if (
+            User.objects.filter(email=email)
+            .exclude(username=username)
+            .exists()
+        ):
+            raise serializers.ValidationError(
+                {
+                    'email': 'This email is already registered with a different username.'
+                }
+            )
+
+        return data
+
+
+class UserSerializer(BaseUserSerializer):
     """
     Serializer for the User model to transform User instances
     to and from JSON. This covers fields like first name, last name,
@@ -27,66 +88,12 @@ class UserSerializer(serializers.ModelSerializer):
         )
 
 
-class UserCreationSerializer(serializers.ModelSerializer):
+class UserCreationSerializer(BaseUserSerializer):
     """
     Serializer to handle user creation. Expects an email and username as input.
     """
 
-    email = serializers.EmailField(required=True)
-    username = serializers.CharField(required=True)
-
-    def validate_email(self, value) -> str:
-        if len(value) > 254:
-            raise serializers.ValidationError(
-                {'email': 'Choose another email'}
-            )
-        return value
-
-    def validate_username(self, value) -> str:
-        if len(value) > 150 or value == 'me':
-            raise serializers.ValidationError(
-                {'username': 'Choose another username'}
-            )
-
-        if not re.match(r'^[\w.@+-]+$', value):
-            raise serializers.ValidationError(
-                'Username contains not allowed symbols'
-            )
-
-        return value
-
-    def validate(self, data):
-        username = data['username']
-        email = data['email']
-
-        if User.objects.filter(username=username).exists():
-            user = User.objects.get(username=username)
-
-            if user.email != email:
-                if not User.objects.filter(email=email).exists():
-                    raise serializers.ValidationError(
-                        {'username': 'Choose another username.'}
-                    )
-                else:
-                    raise serializers.ValidationError(
-                        {
-                            'username': 'Choose another username.',
-                            'email': email,
-                        }
-                    )
-
-        if User.objects.filter(email=email).exists():
-            user = User.objects.get(email=email)
-            if user.username != username:
-                raise serializers.ValidationError(
-                    {
-                        'email': 'This email is already registered with a different username.'
-                    }
-                )
-
-        return data
-
-    def get_or_create(self, **validated_data):
+    def get_or_create(self) -> CustomUser:
         instance, _ = User.objects.get_or_create(
             username=self.validated_data['username'],
             email=self.validated_data['email'],
@@ -110,7 +117,7 @@ class UserAccessTokenSerializer(serializers.ModelSerializer):
     username = serializers.CharField(required=True)
     confirmation_code = serializers.CharField(required=True)
 
-    def validate(self, data):
+    def validate(self, data: OrderedDict) -> OrderedDict:
         user = get_object_or_404(User, username=data['username'])
 
         if not default_token_generator.check_token(
