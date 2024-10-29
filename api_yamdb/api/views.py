b2969ba160bc3921f3bc1e66f942e1e8525bb7b3
Daniil Tropin <Rockur@ya.ru>
2024-10-29 17:14:47 +0300

    Remove unnecessary code

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,148 +1,68 @@
-from django_filters.rest_framework import CharFilter, DjangoFilterBackend, FilterSet
 from django.shortcuts import get_object_or_404
-from rest_framework import filters, status, viewsets
-from rest_framework.response import Response
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import viewsets
 
 from users.permissions import (
     IsSuperuserOrAdmin,
-    IsOwnerOrReadOnly,
     IsAdminOrReadOnly,
-    IsModeratorOrReadOnly
+    IsOwner,
+    IsOwnerOrReadOnly,
 )
 from reviews.models import Category, Genre, Title, Comment
 from api.serializers import (
     CategorySerializer,
     GenreSerializer,
     TitleSerializer,
-    TitleCreateSerializer,
     CommentSerializer,
     ReviewSerializer,
 )
 
 
-class BaseViewSet(viewsets.ModelViewSet):
-    """Base viewset for title, category, genre."""
-
-    filter_backends = (DjangoFilterBackend, filters.SearchFilter)
-
-
-class GenreViewSet(BaseViewSet):
+class GenreViewSet(viewsets.ModelViewSet):
     """Genre viewset."""
 
     queryset = Genre.objects.all().order_by('id')
     lookup_field = 'slug'
     permission_classes = [IsAdminOrReadOnly]
-    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
     serializer_class = GenreSerializer
     search_fields = ('name',)
-
-    def retrieve(self, request, *args, **kwargs):
-        slug = kwargs.get('slug')
-        if not Genre.objects.filter(slug=slug).exists():
-            return Response(
-                {'detail': 'Method Not Allowed'},
-                status=status.HTTP_405_METHOD_NOT_ALLOWED
-            )
-        return super().retrieve(request, *args, **kwargs)
-
-    def destroy(self, request, *args, **kwargs):
-        instance = self.get_object()
-        self.perform_destroy(instance)
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-    def partial_update(self, request, *args, **kwargs):
-        slug = kwargs.get('slug')
-        if not Genre.objects.filter(slug=slug).exists():
-            return Response(
-                {'detail': 'Method Not Allowed'},
-                status=status.HTTP_405_METHOD_NOT_ALLOWED
-            )
-        return super().partial_update(request, *args, **kwargs)
+    http_method_names = ['get', 'post', 'delete']
 
 
-class CategoryViewSet(BaseViewSet):
+class CategoryViewSet(viewsets.ModelViewSet):
     """Category viewset."""
 
     queryset = Category.objects.all().order_by('id')
     serializer_class = CategorySerializer
     lookup_field = 'slug'
     permission_classes = [IsAdminOrReadOnly]
-    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
     search_fields = ('name',)
-
-    def retrieve(self, request, *args, **kwargs):
-        return Response(
-            {'detail': 'Method Not Allowed'},
-            status=status.HTTP_405_METHOD_NOT_ALLOWED
-        )
-
-    def update(self, request, *args, **kwargs):
-        instance = self.get_object()
-        serializer = self.get_serializer(instance, data=request.data, partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_200_OK)
-
-    def partial_update(self, request, *args, **kwargs):
-        return Response(
-            {'detail': 'Method Not Allowed'},
-            status=status.HTTP_405_METHOD_NOT_ALLOWED
-        )
-
-    def destroy(self, request, *args, **kwargs):
-        instance = self.get_object()
-        self.perform_destroy(instance)
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    http_method_names = ['get', 'post', 'patch', 'delete']
 
 
-class TitleFilter(FilterSet):
-    genre = CharFilter(field_name='genre__slug')
-    category = CharFilter(field_name='category__slug')
-
-    class Meta:
-        model = Title
-        fields = ['category', 'genre', 'name', 'year']
-
-
-class TitleViewSet(BaseViewSet):
+class TitleViewSet(viewsets.ModelViewSet):
     """Title viewset."""
 
     queryset = Title.objects.all().order_by('id')
     permission_classes = [IsAdminOrReadOnly]
-    filter_backends = [DjangoFilterBackend]
-    filterset_class = TitleFilter
     serializer_class = TitleSerializer
-    search_fields = ('name', 'category', 'genre', 'year')
+    search_fields = (
+        'name',
+        'category__name',
+        'genre__name',
+        'genre__slug',
+        'year',
+    )
+    filterset_fields = [
+        'name',
+        'year',
+        'category__name',
+        'genre__name',
+        'genre__slug',
+    ]
     http_method_names = ['get', 'post', 'patch', 'delete']
 
-    def get_queryset(self):
-        return Title.objects.all().order_by('id')
-
-    def update(self, request, *args, **kwargs):
-        instance = self.get_object()
-        serializer = self.get_serializer(instance, data=request.data, partial=False)
-        serializer.is_valid(raise_exception=True)
-        self.perform_update(serializer)
-        return Response(serializer.data)
-
-    def partial_update(self, request, *args, **kwargs):
-        instance = self.get_object()
-        serializer = self.get_serializer(instance, data=request.data, partial=True)
-        serializer.is_valid(raise_exception=True)
-        self.perform_update(serializer)
-        return Response(serializer.data)
-
-    def perform_update(self, serializer):
-        serializer.save()
-
-    def get_serializer_class(self):
-        if self.action in ['create', 'partial_update']:
-            return TitleCreateSerializer
-        return TitleSerializer
-
-
-class CommentViewSet(BaseViewSet):
+class CommentViewSet(viewsets.ModelViewSet):
     """ "Comment viewset."""
 
     queryset = Comment.objects.all()
@@ -161,22 +81,19 @@ class ReviewViewSet(viewsets.ModelViewSet):
     """Review viewset."""
 
     serializer_class = ReviewSerializer
-    permission_classes = [IsAdminOrReadOnly, IsOwnerOrReadOnly]
+    permission_classes = [IsSuperuserOrAdmin | IsOwner]
+    # lookup_field = 'title_id'
 
-    def get_title(self) -> Title:
-        return get_object_or_404(Title, pk=self.kwargs.get('title_id'))
+    # def get_title(self) -> Title:
+    #     return get_object_or_404(Title, pk=self.kwargs.get('title_id'))
 
-    def get_queryset(self):
-        return self.get_title().reviews
+    # def get_queryset(self):
+    #     return self.get_title().reviews
 
     def perform_create(self, serializer) -> None:
-        # TODO: connect with custom user
-        # serializer.save(
-        #     title_id=self.kwargs.get('title_id'),
-        #     author=get_user_model().objects.get(pk=1),
-        # )
         serializer.save(
-            title_id=self.kwargs.get('title_id'), author=self.request.user
+            title_id=self.kwargs.get('title_id'),
+            author=self.request.user,
         )
 
     class Meta:
