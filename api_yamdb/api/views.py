9963261d15e283f56e9c7a5c3fc13f92fc53d5b6
Илья Андреев <153600294+Faitik@users.noreply.github.com>
2024-10-31 20:54:10 +0300

    Feature/api views (#34)
    * Change queryset attribute on get_queryset with specific post
    ---------
    
    Co-authored-by: Ilya <Ilya@MakBook-Pro.local>
    Co-authored-by: Daniil Tropin <Rockur@ya.ru>
    Co-authored-by: Daniil <112634689+RockurDev@users.noreply.github.com>

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -57,30 +57,36 @@ class TitleViewSet(viewsets.ModelViewSet):
 class CommentViewSet(viewsets.ModelViewSet):
     """ "Comment viewset."""
 
-    queryset = Comment.objects.all().order_by('-pub_date')
     serializer_class = CommentSerializer
     permission_classes = [IsModeratorOrReadOnly, IsAuthenticatedOrReadOnly]
     http_method_names = ('get', 'post', 'patch', 'delete')
 
     search_fields = ('text',)
 
+    def get_queryset(self):
+        return Comment.objects.all().order_by('-pub_date')
+
     def perform_create(self, serializer: CommentSerializer) -> None:
         title_id = self.kwargs.get('title_id')
         review_id = self.kwargs.get('review_id')
 
-        title = get_object_or_404(Title, id=title_id)
-        review = get_object_or_404(Review, id=review_id)
+        review = get_object_or_404(Review, id=review_id, title_id=title_id)
 
-        serializer.save(title=title, review=review, author=self.request.user)
+        serializer.save(
+            title=review.title, review=review, author=self.request.user
+        )
 
 
 class ReviewViewSet(viewsets.ModelViewSet):
     """Review viewset."""
 
-    queryset = Review.objects.all().order_by('-pub_date')
     serializer_class = ReviewSerializer
     permission_classes = [IsModeratorOrReadOnly, IsAuthenticatedOrReadOnly]
     http_method_names = ('get', 'post', 'patch', 'delete')
+
+    def get_queryset(self):
+        return Review.objects.all().order_by('-pub_date')
+
 
     def perform_create(self, serializer: ReviewSerializer) -> None:
         title = get_object_or_404(Title, id=self.kwargs.get('title_id'))
