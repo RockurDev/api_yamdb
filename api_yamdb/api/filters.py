6038696bdff6cc4c6eb8bbea5346dc802d25b5dd
Chiken-Kitchen <filkargin@yandex.ru>
2024-10-31 15:49:35 +0300

    fix filters, add field=name find on title model.

--- api_yamdb/api/filters.py
+++ api_yamdb/api/filters.py
@@ -1,4 +1,4 @@
-from django_filters import CharFilter, FilterSet
+from django_filters.rest_framework import CharFilter, FilterSet
 
 from reviews.models import Title
 
@@ -6,9 +6,10 @@ from reviews.models import Title
 class TitleFilter(FilterSet):
     """Filter for category, genre."""
 
+    name = CharFilter(field_name='name', lookup_expr='icontains')
     category = CharFilter(field_name='category__slug')
     genre = CharFilter(field_name='genre__slug')
 
     class Meta:
         model = Title
-        fields = ['name', 'category', 'genre', 'year']
+        fields = ('name', 'category', 'genre', 'year')
