57315dcc0ca0294201c89548be511b04cc1be68d
Daniil Tropin <Rockur@ya.ru>
2024-11-01 17:04:17 +0300

    Merge users and apps in api views

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,11 +1,23 @@
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import send_mail
 from django.shortcuts import get_object_or_404
-from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from rest_framework.filters import SearchFilter
 from django_filters.rest_framework import DjangoFilterBackend
-from api.permissions import IsAdminOrReadOnly, IsModeratorOrReadOnly
+from rest_framework import status, viewsets
+from rest_framework.decorators import action, api_view
+from rest_framework.filters import SearchFilter
+from rest_framework.permissions import (
+    IsAuthenticated,
+    IsAuthenticatedOrReadOnly,
+)
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import AccessToken
 
 from .filters import TitleFilter
+from .mixins import GenreCategoryBaseViewSet
+from .permissions import IsSuperuserOrAdmin
 from .serializers import (
     CategorySerializer,
     CommentSerializer,
@@ -13,10 +25,88 @@ from .serializers import (
     ReviewSerializer,
     TitleSerializer,
 )
+from api.permissions import IsAdminOrReadOnly, IsModeratorOrReadOnly
+from api.serializers import UserAccessTokenSerializer, UserSerializer
 from reviews.models import Category, Comment, Genre, Review, Title
-from .mixins import GenreCategoryBaseViewSet
+
+User = get_user_model()
 
 
+# Users
+@api_view(['POST'])
+def signup(request: Request) -> Response:
+    serializer = UserSerializer(data=request.data)
+    # serializer = UserCreationSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+
+    # If the user does not exists and requests confirmation code
+    user = serializer.get_or_create()
+
+    confirmation_code = default_token_generator.make_token(user)
+
+    send_mail(
+        'API_YAMDB. Confirmation code',
+        f'Your confirmation code: {confirmation_code}',
+        settings.DEFAULT_FROM_EMAIL,
+        [user.email],
+        fail_silently=False,
+    )
+
+    return Response(
+        {'username': user.username, 'email': user.email},
+        status=status.HTTP_200_OK,
+    )
+
+
+@api_view(['POST'])
+def get_jwt_token(request: Request) -> Response:
+    serializer = UserAccessTokenSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+
+    username = serializer.validated_data['username']
+    user = get_object_or_404(User, username=username)
+
+    token = AccessToken.for_user(user)
+
+    return Response({'token': str(token)}, status=status.HTTP_200_OK)
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """User viewset."""
+
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsSuperuserOrAdmin]
+    http_method_names = ['get', 'post', 'patch', 'delete']
+    lookup_field = 'username'
+    search_fields = ('role', 'username')
+
+    @action(
+        detail=False,
+        url_path='me',
+        methods=['GET', 'PATCH'],
+        permission_classes=[IsAuthenticated],
+    )
+    def me(self, request: Request) -> Response:
+        if request.method == 'GET':
+            serializer = UserSerializer(self.request.user)
+        else:
+            data = request.data.copy()
+
+            # Prevent role from being changed
+            if 'role' in data and request.user.role != data['role']:
+                data.pop('role')
+
+            serializer = UserSerializer(
+                self.request.user, data=data, partial=True
+            )
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+# Models
 class GenreViewSet(GenreCategoryBaseViewSet):
     """Genre viewset."""
 
