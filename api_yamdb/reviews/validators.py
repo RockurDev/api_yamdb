4b84a3b73d030b7a444a0cde24a302cbe1a6e1e6
Daniil Tropin <Rockur@ya.ru>
2024-11-01 19:08:26 +0300

    Remove Regex validator

--- api_yamdb/reviews/validators.py
+++ api_yamdb/reviews/validators.py
@@ -1,17 +1,7 @@
+from django.core.exceptions import ValidationError
 from django.utils import timezone
 
-from django.core.validators import RegexValidator
-from django.core.exceptions import ValidationError
 
-
-characters_validator = RegexValidator(
-    r'^[-a-zA-Z0-9_]+$',
-    'Latin alphabet symbols, numbers and underscore'
-)
-
-
-def year_validator(value):
+def validate_past_year(value) -> None:
     if value > timezone.now().year:
-        raise ValidationError(
-            'Please enter a valid year!'
-        )
+        raise ValidationError('Please enter a valid year!')
