cae08524c55c38b8423d6b69efc2f3d31c06645b
Daniil Tropin <Rockur@ya.ru>
2024-11-01 13:27:32 +0300

    Update User model name

--- api_yamdb/users/admin.py
+++ api_yamdb/users/admin.py
@@ -2,10 +2,10 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import Group
 
-from .models import CustomUser
+from .models import User
 
 
-@admin.register(CustomUser)
+@admin.register(User)
 class UserAdmin(UserAdmin):
     list_display = (
         'pk',
