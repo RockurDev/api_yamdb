8065cceb02b2f14ab36d11562709bdd8332a78dc
Daniil Tropin <Rockur@ya.ru>
2024-11-02 11:37:52 +0300

    Refactor permissions.py

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -10,10 +10,9 @@ class IsAdminOrReadOnly(permissions.BasePermission):
     """
 
     def has_permission(self, request: Request, view) -> bool:
-        if request.method in permissions.SAFE_METHODS:
-            return True
-
-        return request.user.is_authenticated and request.user.is_admin
+        return request.method in permissions.SAFE_METHODS or (
+            request.user.is_authenticated and request.user.is_admin
+        )
 
 
 class IsModeratorOrReadOnly(permissions.BasePermission):
@@ -40,7 +39,6 @@ class IsSuperuserOrAdmin(permissions.BasePermission):
     """
 
     def has_permission(self, request: Request, view) -> bool:
-        if request.user.is_superuser:
-            return True
-
-        return request.user.is_authenticated and request.user.is_admin
+        return request.user.is_superuser or (
+            request.user.is_authenticated and request.user.is_admin
+        )
