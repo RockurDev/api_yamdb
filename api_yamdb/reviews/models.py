d37ffdf800611dba841b5463d52f3da733abf8c0
Daniil Tropin <Rockur@ya.ru>
2024-11-04 15:01:34 +0300

    Now only tuples to optimize efficiency

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -62,7 +62,7 @@ class Title(models.Model):
         verbose_name='Описание',
     )
     year = models.SmallIntegerField(
-        validators=[validate_past_year],
+        validators=(validate_past_year,),
         verbose_name='Год',
     )
     category = models.ForeignKey(
@@ -81,7 +81,7 @@ class Title(models.Model):
     class Meta:
         verbose_name = 'Произведение'
         verbose_name_plural = 'Произведения'
-        ordering = ['name']
+        ordering = ('name',)
 
     def __str__(self) -> str:
         return self.name[:MAX_NAME_LENGTH]
@@ -105,14 +105,14 @@ class Review(models.Model):
     )
     score = models.IntegerField(
         null=True,
-        validators=[
+        validators=(
             MaxValueValidator(
                 MAX_NUMB, message='The rating should not be higher than 10'
             ),
             MinValueValidator(
                 MIN_NUMB, message='The rating must be at least 1'
             ),
-        ],
+        ),
         verbose_name='Оценка',
     )
     pub_date = models.DateTimeField(
@@ -125,12 +125,12 @@ class Review(models.Model):
     class Meta:
         verbose_name = 'Отзыв'
         verbose_name_plural = 'Отзывы'
-        constraints = [
+        constraints = (
             models.UniqueConstraint(
-                fields=['title', 'author'], name='unique_review'
-            )
-        ]
-        ordering = ['-pub_date']
+                fields=('title', 'author'), name='unique_review'
+            ),
+        )
+        ordering = ('-pub_date',)
 
 
 class Comment(models.Model):
@@ -162,7 +162,7 @@ class Comment(models.Model):
     class Meta:
         verbose_name = 'Комментарий'
         verbose_name_plural = 'Комментарии'
-        ordering = ['-pub_date']
+        ordering = ('-pub_date',)
 
     def __str__(self) -> str:
-        return self.text[MAX_TEXT_LENGTH]
+        return self.text[:MAX_TEXT_LENGTH]
